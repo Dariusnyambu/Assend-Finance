8a256d3e9620906d1f2a275d11a36e0ec5c38b67
Dariusnyambu <150984355+Dariusnyambu@users.noreply.github.com>
2026-03-05 23:20:30 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,58 @@
+// LOAN CALCULATOR
+
 function calculateLoan(){
 
-let amount = document.getElementById("amount").value;
-let months = document.getElementById("months").value;
+let amount=document.getElementById("amount").value;
+let months=document.getElementById("months").value;
 
-let monthlyInterest = amount * 0.30;
+let monthlyInterest=amount*0.30;
+let totalInterest=monthlyInterest*months;
+let totalRepayment=Number(amount)+totalInterest;
 
-let totalInterest = monthlyInterest * months;
-
-let totalRepayment = Number(amount) + totalInterest;
-
-document.getElementById("monthlyInterest").innerText = monthlyInterest;
-document.getElementById("totalInterest").innerText = totalInterest;
-document.getElementById("totalRepayment").innerText = totalRepayment;
+document.getElementById("monthlyInterest").innerText=monthlyInterest;
+document.getElementById("totalInterest").innerText=totalInterest;
+document.getElementById("totalRepayment").innerText=totalRepayment;
 
 }
 
 
-// WHATSAPP FORM
+// HERO SLIDER
+
+let slides=document.querySelectorAll(".slide");
+let index=0;
+
+function showSlides(){
+
+slides.forEach(slide=>{
+slide.classList.remove("active");
+});
+
+index++;
+
+if(index>slides.length){
+index=1;
+}
+
+slides[index-1].classList.add("active");
+
+setTimeout(showSlides,5000);
+
+}
+
+showSlides();
+
+
+// MOBILE MENU
+
+const menuToggle=document.getElementById("menu-toggle");
+const navLinks=document.getElementById("nav-links");
+
+menuToggle.addEventListener("click",()=>{
+navLinks.classList.toggle("active");
+});
+
+
+// FORM WHATSAPP
 
 document.getElementById("loanForm").addEventListener("submit",function(e){
 
@@ -27,6 +63,9 @@ let id=document.getElementById("idNumber").value;
 let phone=document.getElementById("phone").value;
 let amount=document.getElementById("loanAmount").value;
 let purpose=document.getElementById("purpose").value;
+let returnDate=document.getElementById("returnDate").value;
+let guarantorName=document.getElementById("guarantorName").value;
+let guarantorContact=document.getElementById("guarantorContact").value;
 
 let message=
 `Loan Application
@@ -36,11 +75,15 @@ ID: ${id}
 Phone: ${phone}
 
 Loan Amount: ${amount}
-Purpose: ${purpose}`;
+Purpose: ${purpose}
+
+Return Date: ${returnDate}
+
+Guarantor Name: ${guarantorName}
+Guarantor Contact: ${guarantorContact}`;
 
 let url=`https://wa.me/254729983747?text=${encodeURIComponent(message)}`;
 
 window.open(url,"_blank");
-
 
 });
